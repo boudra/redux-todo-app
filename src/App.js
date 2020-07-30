6ddf490823771c8d60a7c067eeaf98f7adc49f4c
Mohamed Boudra <boudra.moha@gmail.com>
2020-07-30 16:36:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+import TodoList from "./TodoList";
+
+const initialState = {
+  todos: [
+    {
+      text: "do the laundry",
+      completed: false
+    }
+  ]
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case "TOGGLE_TODO":
+      return { ...state, todos: state.todos.map((todo, index) => {
+        if(index === action.payload) {
+          return {...todo, completed: !todo.completed};
+        } else {
+          return todo;
+        }
+      })};
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
+store.subscribe(() => console.log("new state", store.getState()))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <TodoList/>
+    </Provider>
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+
+function TodoList({ todos, toggleTodo }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li>
+          <input type="checkbox" onClick={e => toggleTodo(index)} value={completed} />
+          <p>{todo.text}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    todos: state.todos,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    toggleTodo: (index) => dispatch({ type: "TOGGLE_TODO", payload: index }),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
